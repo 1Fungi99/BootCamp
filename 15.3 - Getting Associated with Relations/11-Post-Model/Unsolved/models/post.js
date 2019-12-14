ee7806bd3a3c1d8e991a55c36bf2394787c5b67c
Justin Cheng <ijustin17@gmail.com>
2019-12-14 10:44:48 -0600

    New adds

--- 15.3 - Getting Associated with Relations/11-Post-Model/Unsolved/models/post.js
+++ 15.3 - Getting Associated with Relations/11-Post-Model/Unsolved/models/post.js
@@ -2,4 +2,30 @@ module.exports = function(sequelize, DataTypes) {
   // Add code here to create a Post model
   // This model needs a title, a body, and a category
   // Don't forget to 'return' the post after defining
+
+  var post = sequelize.define("post", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len:[1]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len:[1,140]
+      },
+      defaultValue: "Personal"
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len:[1,140]
+      }
+    }
+  });
+  return post;
 };
